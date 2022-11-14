96dbfd6c499167e220e9a13ecbd3070b2b667a8d
Dipoengoro <dipoengoro@outlook.com>
2022-11-15 05:28:27 +0700

    Step 8

--- app/src/main/java/id/dipoengoro/tryimagesearch/MainActivity.kt
+++ app/src/main/java/id/dipoengoro/tryimagesearch/MainActivity.kt
@@ -2,6 +2,10 @@ package id.dipoengoro.tryimagesearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.core.view.MenuProvider
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
--- app/src/main/java/id/dipoengoro/tryimagesearch/ui/search/SearchFragment.kt
+++ app/src/main/java/id/dipoengoro/tryimagesearch/ui/search/SearchFragment.kt
@@ -1,10 +1,17 @@
 package id.dipoengoro.tryimagesearch.ui.search
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.Lifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import id.dipoengoro.tryimagesearch.R
 import id.dipoengoro.tryimagesearch.databinding.FragmentSearchBinding
@@ -21,6 +28,37 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
         _binding = FragmentSearchBinding.bind(view)
 
         val adapter = UnsplashPhotoAdapter()
+
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_search, menu)
+                val searchItem = menu.findItem(R.id.action_search)
+                val searchView = searchItem.actionView as SearchView
+
+                searchView.setOnQueryTextListener(object : OnQueryTextListener {
+                    override fun onQueryTextSubmit(query: String?): Boolean {
+                        if (query != null) {
+                            binding.recyclerViewSearch.scrollToPosition(0)
+                            viewModel.searchPhotos(query)
+                            searchView.clearFocus()
+                        }
+                        return true
+                    }
+
+                    override fun onQueryTextChange(newText: String?): Boolean = false
+                })
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean =
+                when (menuItem.itemId) {
+                    R.id.action_search -> {
+                        true
+                    }
+                    else -> false
+                }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
         binding.apply {
             recyclerViewSearch.setHasFixedSize(true)
